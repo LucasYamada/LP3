9ff0290710239e32326b9c0b0033da4dd7772343
Estudante 1 <estudante1@debian.spo.ifsp.edu.br>
2024-03-19 08:44:45 -0300

    exercicios

---
+++ aulas/lacos.py
@@ -0,0 +1,18 @@
+palavra = "Python"
+
+for letra in palavra:
+    print(letra)
+
+lista = list(range(0,100))
+
+i=0
+
+numeros = {1,2,3,4,5}
+
+quadrados = {}
+
+quadrados = (numero ** 2 for numero in numeros)
+
+for quadrado in quadrados:
+    print(quadrado)
+
---
+++ exercicios/ex1.py
@@ -0,0 +1,4 @@
+numero = int(input("Digite um numero:"))
+
+print("sucessor:",numero+1)
+print("antecessor:",numero-1)

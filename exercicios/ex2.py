e40229b62ae8bcfe1540b126aef3e5cae4ab5158
LucasYamada <126785678+LucasYamada@users.noreply.github.com>
2024-03-26 19:30:37 -0300

    Update ex2.py

--- exercicios/ex2.py
+++ exercicios/ex2.py
@@ -1,17 +1,13 @@
-compras = int(input("Digite o numero de compras:"))
 valorf = 0
+valori = float(input("Digite o valor da compra:"))
 
-for i in range(0,compras):
-    valori = float(input("Digite o valor da compra:"))
-    if(valori >= 0.01 and valori < 10.0):
-        valorf = valori - valori/25
-    elif(valori >= 10 and valori < 100):
-        valorf = valori - valori/20
-    elif(valori >= 100 and valori < 500):
-        valorf = valori - valori/10
-    else:
-        valorf = valori - valori*0.15
+if(valori >= 0.01 and valori < 10.0):
+    valorf = valori - valori/25
+elif(valori >= 10 and valori < 100):
+    valorf = valori - valori/20
+elif(valori >= 100 and valori < 500):
+    valorf = valori - valori/10
+else:
+    valorf = valori - valori*0.15
 
-    print("O preco com desconto é:",valorf)
-
-print("Obrigado, volte sempre!")
+print("O preco com desconto é:",valorf)
